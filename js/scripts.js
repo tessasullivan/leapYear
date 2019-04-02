2e6de791e4b3b7c944454bcaeb4beab866d59892
Tessa Sullivan and Zema Kebede <tessa@>
2019-04-02 15:55:09 -0700

    Returns true if year is divisible by 4

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function leapYear(year){
+  // Test if divisible by 4
+  if (year % 4 === 0) {
+    console.log(year + " is divisible by 4");
+  }
+  return result;
+}
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    if (result) {
+      $("#result").append("This is divisible by 4");
+    } else {
+      $("#result").append("This is NOT divisible by 4");
+    }
+//    $("#result").text(result);
+  });
+});
